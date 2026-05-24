efbc548465be87da838d96f0a142771f6692088b
estebansar <sar17008@byui.edu>
2026-05-23 21:40:49 -0600

    Update Swagger host for Render

--- swagger.js
+++ swagger.js
@@ -5,8 +5,8 @@ const doc = {
     title: "ToolTrack API",
     description: "API documentation for ToolTrack project"
   },
-  host: "localhost:3000",
-  schemes: ["http"]
+  host: "tooltrack-api.onrender.com",
+  schemes: ["https"]
 }
 
 const outputFile = "./swagger.json"
