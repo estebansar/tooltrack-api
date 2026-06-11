8ab668471f0b1f5faf4548d19d18dc0deef18695
estebansar <sar17008@byui.edu>
2026-06-10 22:33:36 -0600

    Implemented GitHub OAuth login

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,19 @@
 const express = require("express")
 const router = express.Router()
-
 const toolsController = require("../controllers/toolsController")
+const passport = require("../config/passport")
+
+// This route sends the user to GitHub to log in
+router.get("/login", passport.authenticate("github", { scope: ["user:email"] }))
+
+// This route is where GitHub sends the user after login
+router.get(
+  "/github/callback",
+  passport.authenticate("github", { failureRedirect: "/" }),
+  (req, res) => {
+    res.send("You are logged in with GitHub")
+  }
+)
 
 // This route shows all tools
 router.get("/tools", toolsController.getAllTools)
